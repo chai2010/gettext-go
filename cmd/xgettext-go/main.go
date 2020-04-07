a3416838fed893a315130b77fdb40272570b117e
chai2010 <chaishushan@gmail.com>
2020-04-07 21:18:36 +0800

    cmd/xgettext-go: 新的翻译资源提取工具

---
+++ cmd/xgettext-go/main.go
@@ -0,0 +1,12 @@
+// Copyright 2020 ChaiShushan <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// The xgettext-go program extracts translatable strings from Go packages.
+package main
+
+func main() {
+	println("todo")
+}
